0f0434b614b0ce76bfb19eeac710f1aab6b41931
Anthony Brummett <brummett@gmail.com>
2024-06-09 18:16:15 -0500

    Joining a multi-part download when complete is working

--- toshodl/Task/PickFileDownloadSource.py
+++ toshodl/Task/PickFileDownloadSource.py
@@ -4,6 +4,9 @@
 
 import random
 import os
+import asyncio
+import aiofiles
+import hashlib
 
 from toshodl.Task import Task
 from toshodl.Printable import Printable
@@ -60,9 +63,81 @@ class PickFileDownloadSource(Printable, Task):
                 #filename = self.filename + '.' + str(idx)
                 filename = '%s.%03d' % ( self.filename, idx)
 
+            # maybe don't need picker or peers?
             dl_tasks.append( chosen_source_class(url=link, filename=filename, picker=self, peers=dl_tasks) )
+
+        # Join the pieces when they're all done
+        asyncio.create_task(self.finalize_file(dl_tasks))
 
         return dl_tasks
 
     def is_batch(self):
         return self.bundle != self.filename
+
+    async def finalize_file(self, dl_tasks):
+        dl_promises = [ t.done for t in dl_tasks ]
+        try:
+            print(f'{ self.filename } waiting on { len(dl_tasks) } DL tasks...')
+            await asyncio.gather(*dl_promises)
+            print('Done Waiting!')
+        except Exception as e:
+            self.print(f'There was an exception waiting for the pieces of { self.filename }: { e }\n')
+            await self.flush_stdout()
+            return
+
+        if self.is_batch():
+            filename = os.path.join(self.bundle, self.filename)
+        else:
+            filename = self.filename
+
+        dirname = os.path.dirname(filename)
+        try:
+            if dirname:
+                os.makedirs(dirname)
+        except FileExistsError:
+            pass
+
+        if len(dl_tasks) > 1:
+            self.print(f'All parts of { self.filename } are done\n')
+            md5 = await self._join_file_parts(
+                            filename,
+                            [ os.path.join('working', dl_task.filename) for dl_task in dl_tasks ])
+
+        else:
+            self.print(f'{ self.filename } is just one part\n')
+            md5 = await self._move_single_file(
+                            filename,
+                            os.path.join('working', dl_tasks[0].filename))
+
+        if md5.hexdigest() != self.md5:
+            self.print(f'*** { self.filename } md5 differs\n    Got      { md5.hexdigest() }\n    Expected { self.md5 }\n')
+            dirname = os.path.dirname(self.filename)
+            filename = os.path.basename(self.filename)
+            os.rename(self.filename, os.path.join(dirname, f'badsum-{ filename }'))
+
+    async def _join_file_parts(self, filename, parts):
+        async with aiofiles.open(filename, 'wb') as fh:
+            md5 = hashlib.md5()
+            for part in parts:
+                self.print(f'  { part }\n')
+                await self.flush_stdout()
+                async with aiofiles.open(part, 'rb') as part_fh:
+                    chunk = await part_fh.read()
+                    md5.update(chunk)
+                    await fh.write(chunk)
+        self.print(f'  ===> { filename }\n')
+        await self.flush_stdout()
+
+        for part in parts:
+            os.remove(part)
+
+        return md5
+
+    async def _move_single_file(self, filename, dl_filename):
+        md5 = hashlib.md5()
+        async with aiofiles.open(dl_filename, 'rb') as part_fh:
+            chunk = await part_fh.read()
+            md5.update(chunk)
+        os.rename(dl_filename, filename)
+
+        return md5

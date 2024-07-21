e7976e4db3cdc5520ec5cda593ce29c3c358eda3
Anthony Brummett <brummett@gmail.com>
2024-07-21 08:49:23 -0500

    Remove the top-level references to the work queue
    In order to make it possible for a concrete downloader to throw an exception
    signalling it's intent to give up and that the system should try another
    supported downloader, we're going to be refactoring the whole thing to use
    a semaphore to limit the number of simultaneous downloads, rather than the
    workers+queue currently in use.
    
    In the current design, once a job is placed in the queue and started by a
    worker, there's no reasonable way for the original download-picker to catch
    that exception and try another.

--- toshodl.py
+++ toshodl.py
@@ -1,59 +1,16 @@
 #!/usr/bin/env python
 
 import asyncio
-import httpx
-import logging
-import logging.handlers
 import queue
 
 from toshodl.ToshoSearch import ToshoSearch
 from toshodl import AsyncConsole
-from toshodl.Task.ToshoResolver import ToshoResolver
+#from toshodl.Task.ToshoResolver import ToshoResolver
 
-num_workers = 5
-
-# Queue one or more tasks
-# "task" can be None to mean there's nothing to queue
-# A Task instance to queue one task
-# A list of Tasks to queue several
-def queue_tasks(queue, task):
-    if task is None:
-        pass
-    elif isinstance(task, list):
-        for t in task:
-            queue.put_nowait(t)
-    else:
-        queue.put_nowait(task)
-
-async def worker(name, work_queue):
-    while True:
-        task = await work_queue.get()
-        further_tasks = await task.run()
-        queue_tasks(work_queue, further_tasks)
-
-        work_queue.task_done()
-
-def setup_logging():
-    logging.basicConfig()  #level=logging.INFO)
-
-    # Setup for asyncio
-    log_queue = queue.Queue()
-    queue_handler = logging.handlers.QueueHandler(log_queue)
-    log_listener = logging.handlers.QueueListener(log_queue)
-    root_logger = logging.getLogger()
-    root_logger.addHandler(queue_handler)
-    log_listener.start()
+num_downloaders = 5
 
 async def main():
-    setup_logging()
-
-    work_queue = asyncio.Queue()
     reader, writer = await AsyncConsole.init()
-
-    workers = []
-    for i in range(num_workers):
-        w = asyncio.create_task(worker(f'worker-{i}', work_queue))
-        workers.append(w)
 
     tosho = ToshoSearch()
 
@@ -68,8 +25,8 @@ async def main():
             id = await tosho.search(trimmed)
             if id is not None:
                 writer.write(f'{trimmed} is id {id}\n'.encode())
-                task = ToshoResolver(id)
-                queue_tasks(work_queue, task)
+                #task = ToshoResolver(id)
+                #queue_tasks(work_queue, task)
 
     print("Out of the main loop")
 

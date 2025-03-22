058df10d08eae159de9ae442375326f9f5c54e9d
Anthony Brummett <brummett@gmail.com>
2025-03-21 22:00:38 -0500

    Get the KrakenFiles downloader working again
    They instituted a hidden reCaptcha thingy around the first of the year that
    adds another param to the GET that retrieves the DL link (g-recaptcha-response)
    It seems like overkill, but this fake browser is able to fool it enough to
    be able to get the DL link

--- toshodl/KrakenFilesDownloader.py
+++ toshodl/KrakenFilesDownloader.py
@@ -1,53 +1,38 @@
 from io import BytesIO
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+from playwright.async_api import async_playwright
 
 from toshodl.DownloadSourceBase import DownloadSourceBase,XTryAnotherSource
 
 class KrakenFilesDownloader(DownloadSourceBase):
 
-    # magic string needed to submit the download form
-    wk_boundary = '----WebKitFormBoundary7MA4YWxkTrZu0gW'
 
     async def download_from_url(self):
-        response = await self.exception_retry(lambda: self.client.get(self.url))
-        dl_link = await self.get_download_link(response)
+        dl_link = await self.get_download_link()
 
         if dl_link:
             async with self.client.stream('GET', dl_link, timeout=30.0) as response:
                 await self.save_stream_response(response)
 
-    async def get_download_link(self, response):
-        dom = BeautifulSoup(BytesIO(response.content), features='html.parser')
+    async def get_download_link(self):
+        self.print(f"Attempting DL from { self.url }\n")
+        async with async_playwright() as p:
+            browser = await p.firefox.launch()
+            page = await browser.new_page()
+            await page.goto(self.url)
+            self.print(f"Loaded { self.url }\n")
 
-        # There's a form with a download button inside.  Submit the form and get
-        # back a bit of JSON with a URL of the file\
+            form = page.locator('#dl-form')
+            dl_button = form.locator('button[type="submit"]')
 
-        try:
-            form = dom.select_one('#dl-form')
-            dl_token = form.select_one('#dl-token').get('value') # hidden input
-            file_hash = dom.select_one('div[data-file-hash]').get('data-file-hash')
-        except Exception as e:
-            self.print(f"*** Can't parse dl-form at { self.url }: { e }\n")
-            raise XTryAnotherSource
+            async with page.expect_download() as download_info:
+                await dl_button.click()
+                self.print(f"Clicked download for { self.url }\n")
 
-        form_action = urljoin(self.url, form.get('action'))
+            download = await download_info.value
+            dl_url = download.url
+            await download.cancel()
+            await page.close()
 
-        # I expected httpx to properly handle a custom boundary
-        # given structured data like this: data = { 'token': file_hash }
-        # https://github.com/encode/httpx/pull/2278
-        # Maybe the version I'm using doesn't have the change in that PR?
-        payload = f'--{self.wk_boundary}\r\nContent-Disposition: form-data; name="token"\r\n\r\n{dl_token}\r\n--{self.wk_boundary}--'
-        form_response = await self.client.post(form_action,
-                                headers={ 'Content-Type': f'multipart/form-data; boundary={ self.wk_boundary }',
-                                          'cache-control': 'no-cache',
-                                          'hash': file_hash },
-                                data=payload,
-                        )
-
-        dl_info = form_response.json()
-        if dl_info['status'] != 'ok':
-            self.print(f"*** Bad status processing { self.url }: { dl_info }\n")
-            raise XTryAnotherSource
-
-        return dl_info['url']
+        return dl_url
